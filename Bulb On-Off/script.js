4b64f7cdc372d473dc2e8485b95dddf75c9eb643
Tasnia Mahjabin <102618178+tasnia-mahjabin@users.noreply.github.com>
2022-07-28 10:23:15 +0600

    Add files via upload

---
+++ Bulb On-Off/script.js
@@ -0,0 +1,16 @@
+function highlight()
+{document.getElementById('title').innerHTML="hello"}
+highlight()
+
+function getDate()
+{document.getElementById('date').innerHTML=Date()}
+getDate()
+
+function getOnPic()
+{document.getElementById("Turn image").src='images/pic_bulbon.gif'}
+getOnPic()
+
+function getOffPic()
+{document.getElementById("Turn image").src='images/pic_bulboff.gif'}
+getOffPic()
+
